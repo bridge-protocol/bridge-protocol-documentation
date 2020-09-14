c8a78db9565cd9d7929b0278dd6cd88fb7d485d6
Alex Guba <gubanotorious@gmail.com>
2020-09-14 10:07:52 -0400

    Commit to remove documentation from primary SDK project

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("remark-admonitions/styles/infima.css"),
+  require("remark-admonitions/styles/infima.css"),
+  require("infima/dist/css/default/default.css"),
+  require("C:\\Workspaces\\Bridge\\bridge-protocol-documentation\\node_modules\\@docusaurus\\theme-classic\\src\\prism-include-languages"),
+  require("C:\\Workspaces\\Bridge\\bridge-protocol-documentation\\src\\css\\custom.css"),
+];
